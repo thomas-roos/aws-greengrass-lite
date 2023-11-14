4dbf41a72cceb6a59a3445acc2c8f7f54be4f337
Archit Gupta <archigup@amazon.com>
2023-11-13 16:53:51 -0800

    Fix cmake-lint lints
    Skips formatting lints since those will be fixed when we apply the
    formatters to all files.

--- utils/fetchContentFromDeps.cmake
+++ utils/fetchContentFromDeps.cmake
@@ -4,6 +4,7 @@ include(FetchContent)
 
 file(READ "${CMAKE_CURRENT_LIST_DIR}/../dependencies.json" PROJECT_DEPS_JSON)
 
+# Function to get FetchContent declarations from dependencies.json
 function(fetchContentFromDeps)
     foreach(name IN LISTS ARGN)
         string(JSON url GET "${PROJECT_DEPS_JSON}" "${name}" url)
