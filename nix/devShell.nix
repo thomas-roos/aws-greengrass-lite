819927c442c672440ae3ef25cf8fe73655ff2002
Archit Gupta <archigup@amazon.com>
2024-01-25 16:08:23 -0800

    Don't pull in GDB on macs
    GDB does not support MacOS, so it should be skipped on that platform.

--- nix/devShell.nix
+++ nix/devShell.nix
@@ -1,16 +1,17 @@
-{
+{ lib, ... }: {
   inputsFrom = pkgs: [ pkgs.default ];
-  packages = pkgs: with pkgs; [
+  packages = pkgs: with pkgs; ([
     coreutils
     clang-tools_16
-    gdb
     cppcheck
     cmake-format
     fd
     git
     git-secrets
     (python3.withPackages (ps: with ps; [ yapf python-lsp-server ]))
-  ];
+  ] ++ (lib.optionals (!pkgs.stdenv.isDarwin) [
+    gdb
+  ]));
   env = {
     CMAKE_EXPORT_COMPILE_COMMANDS = "1";
     NIX_HARDENING_ENABLE = "";
