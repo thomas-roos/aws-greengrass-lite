013a7a5330c0a6d4f275c2d2bb761c417c626c0f
Archit Gupta <archigup@amazon.com>
2024-02-27 02:11:23 -0800

    Change install dir to `install`

--- nix/apps/configure.nix
+++ nix/apps/configure.nix
@@ -1,6 +1,6 @@
 pkgs: ''
   nix develop -c cmake -Bbuild \
     -DCMAKE_BUILD_TYPE=Debug \
-    -DCMAKE_INSTALL_PREFIX=$PWD/run \
+    -DCMAKE_INSTALL_PREFIX=$PWD/install \
     ${toString pkgs.default.fetchcontentFlags}
 ''
