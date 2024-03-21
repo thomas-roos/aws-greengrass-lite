0cbd8e27c888231bff234cb0436e391c19d324a5
Archit Gupta <archigup@amazon.com>
2024-03-21 02:33:07 -0700

    Split Nix build for caching build dir

--- utils/nix/packages/_default.nix
+++ utils/nix/packages/_default.nix
@@ -5,45 +5,117 @@
 , ninja
 , openssl
 , src
+, runCommand
+, gnutar
 , darwin ? null
 }:
 let
   inherit (builtins) fromJSON readFile;
-  inherit (lib) fileset mapAttrs;
+  inherit (lib) any concatStrings fileset mapAttrs;
+
   deps = mapAttrs (_: v: fetchgit (v // { fetchSubmodules = true; }))
     (fromJSON (readFile (src + "/dependencies.json")));
   fetchcontentFlags = lib.mapAttrsToList
     (n: v: "-DFETCHCONTENT_SOURCE_DIR_${lib.toUpper n}=${v}")
     deps;
-in
-stdenv.mkDerivation {
-  pname = "gglite";
-  version = "0.0.1";
-  src = fileset.toSource {
-    root = src;
-    fileset = fileset.unions [
-      (src + "/CMakeLists.txt")
-      (src + "/common.cmake")
-      (src + "/dependencies.json")
-      (src + "/nucleus")
-      (src + "/plugin_api")
-      (src + "/plugins")
-      (src + "/utils/cmake")
+
+  # All files needed for build
+  baseFileset = fileset.unions (map (f: src + f) [
+    "/CMakeLists.txt"
+    "/common.cmake"
+    "/dependencies.json"
+    "/nucleus"
+    "/plugin_api"
+    "/plugins"
+    "/utils/cmake"
+  ]);
+
+  # Files used by cmake to configure
+  cmakeFileset = fileset.intersection
+    baseFileset
+    (fileset.fileFilter
+      (file: any file.hasExt [ "txt" "cmake" "json" "yaml" "yml" ])
+      src);
+
+  # Files checked by cmake but not used
+  cmakeExtraFiles = map (lib.path.removePrefix src)
+    (fileset.toList (fileset.difference baseFileset cmakeFileset));
+
+  # Source with cmake-used files, and with empty cmake-checked files
+  # Empty source files are needed since cmake fails if sources are not found
+  # Using empty files prevents rebuilds when the actual source file changes
+  cmakeSrc = runCommand "source" { } (''
+    cp -r ${fileset.toSource { root = src; fileset = cmakeFileset; }} $out
+    chmod -R +w $out
+    cd $out
+  '' + (concatStrings (map
+    (path: ''
+      mkdir -p "$(dirname "${path}")"
+      touch "${path}"
+    '')
+    cmakeExtraFiles)));
+
+  commonArgs = {
+    version = "0.0.1";
+    strictDeps = true;
+    nativeBuildInputs = [ cmake ninja ];
+    buildInputs = [ openssl ];
+    hardeningDisable = [ "all" ];
+    cmakeBuildType = "MinSizeRel";
+    cmakeDir = "/build/source";
+    cmakeBuildDir = "/tmp/build";
+    cmakeFlags = fetchcontentFlags ++ [
+      "-GNinja"
+      "-DBUILD_TESTING=1"
+      "-DCMAKE_INSTALL_PREFIX=/tmp/install"
     ];
+    dontAddPrefix = true;
+  } // lib.optionalAttrs stdenv.isDarwin {
+    buildInputs = [ openssl darwin.Security ];
+    cmakeBuildType = "Debug";
+    postUnpack = ''
+      rm -rf /tmp/source
+      cp -r source /tmp
+      cmakeDir=/tmp/source
+    '';
   };
-  strictDeps = true;
-  nativeBuildInputs = [ cmake ninja ];
-  buildInputs = [ openssl ] ++ lib.optional stdenv.isDarwin darwin.Security;
-  hardeningDisable = [ "all" ];
-  cmakeBuildType = if stdenv.isDarwin then "Debug" else "MinSizeRel";
-  cmakeFlags = fetchcontentFlags ++ [
-    "-DBUILD_TESTING=1"
-  ];
+
+  # This intermediate drv is not reproducible :(
+  buildDir = stdenv.mkDerivation (commonArgs // {
+    pname = "gglite-build";
+    src = cmakeSrc;
+    # cache dependencies
+    ninjaFlags = [
+      "aws-crt-cpp"
+      "IotIdentity-cpp"
+      "aws-c-http"
+      "aws-c-event-stream"
+      "yaml-cpp"
+      "Catch2WithMain"
+    ];
+    installPhase = ''
+      ${gnutar}/bin/tar -czf $out --posix --sort=name \
+        --numeric-owner --owner=0 --group=0 -- .
+    '';
+    dontFixup = true;
+  });
+in
+stdenv.mkDerivation (commonArgs // {
+  pname = "gglite";
+  src = fileset.toSource { root = src; fileset = baseFileset; };
+  configurePhase = ''
+    mkdir -p /tmp/build
+    cd /tmp/build
+    ${gnutar}/bin/tar -zxf ${buildDir}
+  '';
   doCheck = true;
   checkPhase = ''
     runHook preCheck
     ctest -R nucleus --output-on-failure
     runHook postCheck
   '';
+  postInstall = ''
+    mv /tmp/install $out
+  '';
   passthru = { inherit fetchcontentFlags; };
-}
+})
