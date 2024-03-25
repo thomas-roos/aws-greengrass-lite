8802de9ab300d907883c629d6ed129378079ee8e
Archit Gupta <archigup@amazon.com>
2024-03-25 12:30:50 -0700

    Split Nix checks into directory

--- utils/nix/checks.nix
+++
@@ -1,7 +0,0 @@
-{
-  cmake-lint = pkgs: ''
-    ${pkgs.cmake-format}/bin/cmake-lint \
-      $(${pkgs.fd}/bin/fd '.*\.cmake|CMakeLists.txt') \
-      -c .cmake-format.yml --suppress-decorations
-  '';
-}
---
+++ utils/nix/checks/cmake-lint.nix
@@ -0,0 +1,5 @@
+pkgs: ''
+  ${pkgs.cmake-format}/bin/cmake-lint \
+    $(${pkgs.fd}/bin/fd '.*\.cmake|CMakeLists.txt') \
+    -c .cmake-format.yml --suppress-decorations
+''
