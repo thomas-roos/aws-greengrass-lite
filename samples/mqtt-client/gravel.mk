018dd907a659475cd09455de16c7502b8a42b850
Archit Gupta <archigup@amazon.com>
2024-05-15 11:25:16 -0700

    Add test client for iotcored

---
+++ samples/mqtt-client/gravel.mk
@@ -0,0 +1,5 @@
+# gravel - Utilities for AWS IoT Core clients
+# Copyright (C) 2024 Amazon.com, Inc. or its affiliates
+
+samples/mqtt-client_LIBS := gravel-lib msgpack-rpc
+samples/mqtt-client_BIN := mqtt-client
---
+++ samples/mqtt-client/src/main.c
@@ -0,0 +1,34 @@
+/* gravel - Utilities for AWS IoT Core clients
+ * Copyright (C) 2024 Amazon.com, Inc. or its affiliates
+ */
+
+#include "gravel/client.h"
+#include "gravel/log.h"
+#include "gravel/object.h"
+#include <errno.h>
+#include <unistd.h>
+
+int main(void) {
+    GravelBuffer iotcored = GRAVEL_STR("/aws/gravel/iotcored");
+
+    GravelConn *conn;
+    int ret = gravel_connect(iotcored, &conn);
+    if (ret != 0) {
+        GRAVEL_LOGE(
+            "mqtt-client",
+            "Failed to connect to %.*s",
+            (int) iotcored.len,
+            iotcored.data
+        );
+        return EHOSTUNREACH;
+    }
+
+    GravelList args = GRAVEL_LIST(GRAVEL_OBJ_MAP(
+        { GRAVEL_STR("topic"), GRAVEL_OBJ_STR("hello") },
+        { GRAVEL_STR("payload"), GRAVEL_OBJ_STR("hello world") },
+    ));
+
+    gravel_notify(conn, GRAVEL_STR("publish"), args);
+
+    GRAVEL_LOGI("mqtt-client", "Sent MQTT publish.");
+}
