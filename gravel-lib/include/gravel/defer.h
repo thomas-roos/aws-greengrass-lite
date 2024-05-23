b6dc2f1bea9256c8d6f862421d58714d424bd132
Archit Gupta <archigup@amazon.com>
2024-05-23 10:52:01 -0700

    Separate joined macro idents with two underscores
    This makes the defer macros generate ids like `gravel_defer__free`
    instead of `gravel_defer_free` making it easier to read the macro
    expansion.

--- gravel-lib/include/gravel/defer.h
+++ gravel-lib/include/gravel/defer.h
@@ -16,41 +16,41 @@
 // clang-format off
 /** Run clean up for `fn` when this falls out of scope. */
 #define GRAVEL_DEFER(fn, ...) \
-    __attribute__((cleanup(gravel_defer_##fn))) \
-    GravelDeferRecord_##fn \
+    __attribute__((cleanup(gravel_defer__##fn))) \
+    GravelDeferRecord__##fn \
     GRAVEL_MACRO_PASTE(\
-        gravel_defer_record_, GRAVEL_MACRO_FIRST(__VA_ARGS__, ) \
+        gravel_defer_record__, GRAVEL_MACRO_FIRST(__VA_ARGS__, ) \
     ) = { &GRAVEL_MACRO_FIRST(__VA_ARGS__, ), \
           GRAVEL_MACRO_REST(__VA_ARGS__, ) }; \
     static void (* const GRAVEL_MACRO_PASTE( \
-        gravel_defer_fn_, GRAVEL_MACRO_FIRST(__VA_ARGS__, ) \
-    ))(GravelDeferRecord_##fn *) __attribute__((unused)) = \
-        gravel_defer_##fn; \
+        gravel_defer_fn__, GRAVEL_MACRO_FIRST(__VA_ARGS__, ) \
+    ))(GravelDeferRecord__##fn *) __attribute__((unused)) = \
+        gravel_defer__##fn; \
     static void (* const GRAVEL_MACRO_PASTE( \
-        gravel_defer_cancel_fn_, GRAVEL_MACRO_FIRST(__VA_ARGS__, ) \
-    ))(GravelDeferRecord_##fn *) __attribute__((unused)) = \
-        gravel_cancel_defer_##fn
+        gravel_defer_cancel_fn__, GRAVEL_MACRO_FIRST(__VA_ARGS__, ) \
+    ))(GravelDeferRecord__##fn *) __attribute__((unused)) = \
+        gravel_cancel_defer__##fn
 // clang-format on
 
 /** Cancel cleanup for `id`. */
 #define GRAVEL_DEFER_CANCEL(id) \
-    gravel_defer_cancel_fn_##id(&gravel_defer_record_##id)
+    gravel_defer_cancel_fn__##id(&gravel_defer_record__##id)
 
 /** Immediately cleanup `id`. */
 #define GRAVEL_DEFER_FORCE(id) \
-    gravel_defer_fn_##id(&gravel_defer_record_##id); \
+    gravel_defer_fn__##id(&gravel_defer_record__##id); \
     GRAVEL_DEFER_CANCEL(id)
 
 /** Enable defer for a function with one parameter. */
 #define GRAVEL_DEFINE_DEFER(fn, type, name, cleanup) \
-    typedef type *GravelDeferRecord_##fn; \
-    static inline void gravel_defer_##fn(type **defer_record_ptr) { \
+    typedef type *GravelDeferRecord__##fn; \
+    static inline void gravel_defer__##fn(type **defer_record_ptr) { \
         type *name = *defer_record_ptr; \
         if (name != NULL) { \
             cleanup; \
         } \
     } \
-    static inline void gravel_cancel_defer_##fn(type **defer_record_ptr) { \
+    static inline void gravel_cancel_defer__##fn(type **defer_record_ptr) { \
         *defer_record_ptr = NULL; \
     }
 
@@ -72,15 +72,15 @@ GRAVEL_DEFINE_DEFER(
 typedef struct {
     void **ptr;
     GravelAlloc *ctx;
-} GravelDeferRecord_gravel_free;
+} GravelDeferRecord__gravel_free;
 
 static inline void
-gravel_defer_gravel_free(GravelDeferRecord_gravel_free *record) {
+gravel_defer__gravel_free(GravelDeferRecord__gravel_free *record) {
     gravel_free(record->ctx, *record->ptr);
 }
 
 static inline void
-gravel_cancel_defer_gravel_free(GravelDeferRecord_gravel_free *record) {
+gravel_cancel_defer__gravel_free(GravelDeferRecord__gravel_free *record) {
     *record->ptr = NULL;
 }
 
