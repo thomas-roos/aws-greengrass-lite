9a7af3372391ad471a58262abfe9e86e9faf36f1
Archit Gupta <archigup@amazon.com>
2024-06-25 15:42:42 -0700

    Add clang-tidy check

--- flake.nix
+++ flake.nix
@@ -10,17 +10,18 @@
       llvmStdenvFor = pkgs: with pkgs; overrideCC llvmPackages.stdenv
         (llvmPackages.stdenv.cc.override
           { inherit (llvmPackages) bintools; });
+
+      core_mqtt = { fetchgit, ... }: fetchgit {
+        url = "https://github.com/FreeRTOS/coreMQTT.git";
+        rev = "58d626a25839ed20b19a57fdff34a41f36dacd05";
+        hash = "sha256-QJyK0LGPCFMx6Q1/VyNBS8m5ywAiQcbgLdPRkA3+ilw=";
+      };
     in
     rec {
       pname = "gravel";
       package =
-        { lib, stdenv, fetchgit, runCommand, pkg-config, openssl, defaultMeta }:
+        { pkgs, lib, stdenv, fetchgit, runCommand, pkg-config, openssl, defaultMeta }:
         let
-          core_mqtt = fetchgit {
-            url = "https://github.com/FreeRTOS/coreMQTT.git";
-            rev = "58d626a25839ed20b19a57fdff34a41f36dacd05";
-            hash = "sha256-QJyK0LGPCFMx6Q1/VyNBS8m5ywAiQcbgLdPRkA3+ilw=";
-          };
           filtered-src = lib.fileset.toSource {
             root = ./.;
             fileset = lib.fileset.fileFilter
@@ -30,7 +31,7 @@
           src-with-modules = runCommand "gravel-src" { } ''
             cp -r ${filtered-src} $out
             chmod -R +w $out
-            ln -s ${core_mqtt} $out/deps/core_mqtt/coreMQTT
+            ln -s ${core_mqtt pkgs} $out/deps/core_mqtt/coreMQTT
           '';
         in
         stdenv.mkDerivation {
@@ -62,10 +63,34 @@
 
       devShells.clang = devShell // { stdenv = llvmStdenvFor; };
 
-      checks.spelling = pkgs: ''
-        ${pkgs.nodePackages.cspell}/bin/cspell "**" --quiet
-        ${pkgs.coreutils}/bin/sort -cuf utils/dictionary.txt
-      '';
+      checks = {
+        spelling = pkgs: ''
+          ${pkgs.nodePackages.cspell}/bin/cspell "**" --quiet
+          ${pkgs.coreutils}/bin/sort -cuf utils/dictionary.txt
+        '';
+        clang-tidy =
+          { pkgs, stdenv, runCommand, pkg-config, bear, clang-tools, openssl, ... }:
+          let
+            src-with-modules = runCommand "gravel-src" { } ''
+              cp -r ${./.} $out
+              chmod -R +w $out
+              ln -s ${core_mqtt pkgs} $out/deps/core_mqtt/coreMQTT
+            '';
+          in
+          stdenv.mkDerivation {
+            name = "check-clang-tidy";
+            src = src-with-modules;
+            nativeBuildInputs = [ pkg-config bear clang-tools ];
+            buildInputs = [ openssl ];
+            preConfigure = "export AR=gcc-ar";
+            buildPhase = ''
+              make PROFILE=debug compile_commands.json
+              clang-tidy $(find . -name "*.c" -not -path "./deps/*") \
+                --warnings-as-errors='*'
+            '';
+            installPhase = "touch $out";
+          };
+      };
 
       formatters = pkgs: {
         "*.c" = "${pkgs.llvmPackages.clang-unwrapped}/bin/clang-format -i";
