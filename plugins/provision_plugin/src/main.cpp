12c6ebd329b194cd47a467540af5907826947a03
rawalexe <rawalexe@amazon.com>
2024-06-25 18:44:33 +0000

    remove provision_plugin

--- plugins/provision_plugin/src/main.cpp
+++
@@ -1,6 +0,0 @@
-#include "provision_plugin.hpp"
-
-extern "C" [[maybe_unused]] ggapiErrorKind greengrass_lifecycle(
-    ggapiObjHandle moduleHandle, ggapiSymbol phase, ggapiObjHandle data) noexcept {
-    return ProvisionPlugin::get().lifecycle(moduleHandle, phase, data);
-}
