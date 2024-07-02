5beb26f9a091c9a989a659eb4a37aab53b0968f8
Archit Gupta <archigup@amazon.com>
2024-07-01 18:29:58 -0700

    Add argp-standalone to Nix build environment

--- nix/checks/clang-tidy.nix
+++ nix/checks/clang-tidy.nix
@@ -1,9 +1,16 @@
-{ ggl-util, pkg-config, bear, clang-tools, openssl, ... }:
+{ ggl-util
+, pkg-config
+, bear
+, clang-tools
+, openssl
+, argp-standalone
+, ...
+}:
 ggl-util.llvmStdenv.mkDerivation {
   name = "check-clang-tidy";
   src = ggl-util.fixedSrc;
   nativeBuildInputs = [ pkg-config bear clang-tools ];
-  buildInputs = [ openssl ];
+  buildInputs = [ openssl argp-standalone ];
   buildPhase = ''
     make PROFILE=debug compile_commands.json
     clang-tidy $(find . -name "*.c" -not -path "./deps/*") \
--- nix/package.nix
+++ nix/package.nix
@@ -2,13 +2,14 @@
 , stdenv
 , pkg-config
 , openssl
+, argp-standalone
 , defaultMeta
 }:
 stdenv.mkDerivation {
   name = "ggl";
   src = ggl-util.fixedSrc;
   nativeBuildInputs = [ pkg-config ];
-  buildInputs = [ openssl ];
+  buildInputs = [ openssl argp-standalone ];
   preConfigure = "export AR=gcc-ar";
   installFlags = [ "DESTDIR=$(out)" ];
   meta = defaultMeta;
