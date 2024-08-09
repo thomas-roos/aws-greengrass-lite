2ed7a74ea1b9d0c10e8e0ff12c1a80d750d38aed
Archit Gupta <archigup@amazon.com>
2024-08-09 00:16:35 -0700

    Add IPC PublishToTopic support

---
+++ ggipcd/src/handlers/publish_to_topic.c
@@ -0,0 +1,117 @@
+// aws-greengrass-lite - AWS IoT Greengrass runtime for constrained devices
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+#include "../ipc_server.h"
+#include "handlers.h"
+#include <ggl/alloc.h>
+#include <ggl/core_bus/client.h>
+#include <ggl/error.h>
+#include <ggl/log.h>
+#include <ggl/map.h>
+#include <ggl/object.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdlib.h>
+
+GglError handle_publish_to_topic(
+    GglMap args, uint32_t handle, int32_t stream_id, GglAlloc *alloc
+) {
+    (void) alloc;
+
+    GglObject *val = NULL;
+    bool found = ggl_map_get(args, GGL_STR("topic"), &val);
+    if (!found) {
+        GGL_LOGE("PublishToTopic", "Missing topic.");
+        return GGL_ERR_INVALID;
+    }
+    if (val->type != GGL_TYPE_BUF) {
+        GGL_LOGE("PublishToTopic", "topic not a string.");
+        return GGL_ERR_INVALID;
+    }
+    GglBuffer topic = val->buf;
+
+    found = ggl_map_get(args, GGL_STR("publishMessage"), &val);
+    if (!found) {
+        GGL_LOGE("PublishToTopic", "Missing publishMessage.");
+        return GGL_ERR_INVALID;
+    }
+    if (val->type != GGL_TYPE_MAP) {
+        GGL_LOGE("PublishToTopic", "publishMessage not a map.");
+        return GGL_ERR_INVALID;
+    }
+    GglMap publish_message = val->map;
+
+    bool is_json;
+
+    found = ggl_map_get(publish_message, GGL_STR("jsonMessage"), &val);
+    if (found) {
+        found = ggl_map_get(publish_message, GGL_STR("binaryMessage"), NULL);
+        if (found) {
+            GGL_LOGE(
+                "PublishToTopic",
+                "publishMessage has both binaryMessage and jsonMessage."
+            );
+            return GGL_ERR_INVALID;
+        }
+
+        is_json = true;
+    } else {
+        found = ggl_map_get(publish_message, GGL_STR("binaryMessage"), &val);
+        if (!found) {
+            GGL_LOGE(
+                "PublishToTopic",
+                "publishMessage missing binaryMessage or jsonMessage."
+            );
+            return GGL_ERR_INVALID;
+        }
+
+        is_json = false;
+    }
+
+    if (val->type != GGL_TYPE_MAP) {
+        GGL_LOGE(
+            "PublishToTopic",
+            "%sMessage not a map.",
+            is_json ? "json" : "binary"
+        );
+        return GGL_ERR_INVALID;
+    }
+
+    found = ggl_map_get(val->map, GGL_STR("message"), &val);
+    if (!found) {
+        GGL_LOGE(
+            "PublishToTopic",
+            "Missing message in %sMessage.",
+            is_json ? "json" : "binary"
+        );
+        return GGL_ERR_INVALID;
+    }
+    if (val->type != GGL_TYPE_BUF) {
+        GGL_LOGE("PublishToTopic", "message is not a string.");
+        return GGL_ERR_INVALID;
+    }
+
+    GglBuffer message = val->buf;
+
+    GglMap call_args = GGL_MAP(
+        { GGL_STR("topic"), GGL_OBJ(topic) },
+        { GGL_STR("type"),
+          is_json ? GGL_OBJ_STR("json") : GGL_OBJ_STR("base64") },
+        { GGL_STR("message"), GGL_OBJ(message) },
+    );
+
+    GglError ret = ggl_call(
+        GGL_STR("pubsub"), GGL_STR("publish"), call_args, NULL, NULL, NULL
+    );
+    if (ret != GGL_ERR_OK) {
+        return ret;
+    }
+
+    return ggl_ipc_response_send(
+        handle,
+        stream_id,
+        GGL_STR("aws.greengrass#PublishToTopicResponse"),
+        GGL_OBJ_MAP()
+    );
+}
--- ggipcd/src/ipc_dispatch.c
+++ ggipcd/src/ipc_dispatch.c
@@ -20,29 +20,30 @@ uint8_t ggl_ipc_handler_resp_mem
      + GGL_IPC_MAX_MSG_LEN];
 pthread_mutex_t ggl_ipc_handler_resp_mem_mtx = PTHREAD_MUTEX_INITIALIZER;
 
+static const struct {
+    GglBuffer operation;
+    GglIpcHandler *handler;
+} HANDLER_TABLE[] = {
+    { GGL_STR("aws.greengrass#PublishToIoTCore"), handle_publish_to_iot_core },
+    { GGL_STR("aws.greengrass#SubscribeToIoTCore"),
+      handle_subscribe_to_iot_core },
+    { GGL_STR("aws.greengrass#PublishToTopic"), handle_publish_to_topic },
+};
+
+static const size_t HANDLER_COUNT
+    = sizeof(HANDLER_TABLE) / sizeof(HANDLER_TABLE[0]);
+
 GglError ggl_ipc_handle_operation(
     GglBuffer operation, GglMap args, uint32_t handle, int32_t stream_id
 ) {
-    struct {
-        GglBuffer operation;
-        GglIpcHandler *handler;
-    } handler_table[] = {
-        { GGL_STR("aws.greengrass#PublishToIoTCore"),
-          handle_publish_to_iot_core },
-        { GGL_STR("aws.greengrass#SubscribeToIoTCore"),
-          handle_subscribe_to_iot_core },
-    };
-
-    size_t handler_count = sizeof(handler_table) / sizeof(handler_table[0]);
-
-    for (size_t i = 0; i < handler_count; i++) {
-        if (ggl_buffer_eq(operation, handler_table[i].operation)) {
+    for (size_t i = 0; i < HANDLER_COUNT; i++) {
+        if (ggl_buffer_eq(operation, HANDLER_TABLE[i].operation)) {
             pthread_mutex_lock(&ggl_ipc_handler_resp_mem_mtx);
             GGL_DEFER(pthread_mutex_unlock, ggl_ipc_handler_resp_mem_mtx);
             GglBumpAlloc balloc
                 = ggl_bump_alloc_init(GGL_BUF(ggl_ipc_handler_resp_mem));
 
-            return handler_table[i].handler(
+            return HANDLER_TABLE[i].handler(
                 args, handle, stream_id, &balloc.alloc
             );
         }
