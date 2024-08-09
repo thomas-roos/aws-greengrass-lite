4dac63e4375998c5e8160ae8d14d915dc1de17ee
Archit Gupta <archigup@amazon.com>
2024-08-09 12:19:25 -0700

    Parallelize clang-tidy check

--- nix/checks/clang-tidy.nix
+++ nix/checks/clang-tidy.nix
@@ -14,15 +14,44 @@
 let
   c-files = map (p: lib.removePrefix ((toString src) + "/") (toString p))
     (lib.fileset.toList (lib.fileset.fileFilter (file: file.hasExt "c") src));
+
+  build-dir = ggl-util.llvmStdenv.mkDerivation {
+    name = "clang-tidy-build-dir";
+    src = ggl-util.fixedSrc;
+    nativeBuildInputs = [ pkg-config clang-tools ];
+    buildInputs = [ openssl curl argp-standalone sqlite.dev libyaml.dev ];
+    buildPhase = ''
+      ${cmake}/bin/cmake -B $out -D CMAKE_BUILD_TYPE=Debug
+      rm $out/CMakeFiles/CMakeConfigureLog.yaml
+    '';
+    dontPatch = true;
+    dontConfigure = true;
+    dontInstall = true;
+    dontFixup = true;
+  };
+
+  check-clang-tidy = file: ggl-util.llvmStdenv.mkDerivation {
+    name = "clang-tidy-${file}";
+    src = ggl-util.fixedSrc;
+    nativeBuildInputs = [ pkg-config clang-tools ];
+    buildInputs = [ openssl curl argp-standalone sqlite.dev libyaml.dev ];
+    buildPhase = ''
+      clang-tidy -p ${build-dir} --quiet --warnings-as-errors='*' ${file}
+      touch $out
+    '';
+    dontPatch = true;
+    dontConfigure = true;
+    dontInstall = true;
+    dontFixup = true;
+  };
 in
 ggl-util.llvmStdenv.mkDerivation {
   name = "check-clang-tidy";
-  src = ggl-util.fixedSrc;
-  nativeBuildInputs = [ pkg-config clang-tools ];
-  buildInputs = [ openssl curl argp-standalone sqlite.dev libyaml.dev ];
-  buildPhase = ''
-    ${cmake}/bin/cmake -B build -D CMAKE_BUILD_TYPE=Debug
-    clang-tidy -p build --warnings-as-errors='*' ${toString c-files}
-  '';
+  buildInputs = map check-clang-tidy c-files;
   installPhase = "touch $out";
+  dontUnpack = true;
+  dontPatch = true;
+  dontConfigure = true;
+  dontBuild = true;
+  dontFixup = true;
 }
