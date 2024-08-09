68efd606c9fad3d986859df99d84f9905910d62f
rawalexe <rawalexe@amazon.com>
2024-08-09 22:32:44 +0000

    Create TES-test for end to end test

---
+++ tesd-test/bin/tesd-test.c
@@ -0,0 +1,15 @@
+// aws-greengrass-lite - AWS IoT Greengrass runtime for constrained devices
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+// tesd -- Token Exchange Service for AWS credential desperse management
+
+#include "tesd-test.h"
+#include <ggl/error.h>
+
+int main(void) {
+    GglError ret = run_tesd_test();
+    if (ret != GGL_ERR_OK) {
+        return 1;
+    }
+}
---
+++ tesd-test/src/entry.c
@@ -0,0 +1,43 @@
+// aws-greengrass-lite - AWS IoT Greengrass runtime for constrained devices
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+#include "tesd-test.h"
+#include <ggl/bump_alloc.h>
+#include <ggl/core_bus/client.h>
+#include <ggl/error.h>
+#include <ggl/log.h>
+#include <ggl/object.h>
+#include <stdint.h>
+#include <stdio.h>
+
+GglError run_tesd_test(void) {
+    static GglBuffer tesd = GGL_STR("/aws/ggl/tesd");
+
+    GglObject result;
+    GglMap params = { 0 };
+    static uint8_t big_buffer_for_bump[4096];
+    GglBumpAlloc the_allocator
+        = ggl_bump_alloc_init(GGL_BUF(big_buffer_for_bump));
+
+    GglError error = ggl_call(
+        tesd,
+        GGL_STR("request_credentials"),
+        params,
+        NULL,
+        &the_allocator.alloc,
+        &result
+    );
+    if (error != GGL_ERR_OK) {
+        return GGL_ERR_FAILURE;
+    }
+
+    GGL_LOGE(
+        "request_credentials",
+        "Received token, sessionToken: %.*s",
+        (int) result.map.pairs[2].val.buf.len,
+        result.map.pairs[2].val.buf.data
+    );
+
+    return GGL_ERR_OK;
+}
