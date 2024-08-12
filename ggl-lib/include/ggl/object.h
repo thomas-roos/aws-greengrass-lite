881e6d81240d33933ec9c1af07a62868ab226109
rawalexe <rawalexe@amazon.com>
2024-08-12 16:27:21 +0000

    Add support for converting a const char * as well to GglBuffer type

--- ggl-lib/include/ggl/object.h
+++ ggl-lib/include/ggl/object.h
@@ -71,7 +71,9 @@ typedef struct GglKV {
     _Generic( \
         (&(strlit)), \
         char(*)[]: ((GglBuffer) { .data = (uint8_t *) (strlit), \
-                                  .len = sizeof(strlit) - 1U }) \
+                                  .len = sizeof(strlit) - 1U }), \
+        const char(*)[]: ((GglBuffer) { .data = (uint8_t *) (strlit), \
+                                        .len = sizeof(strlit) - 1U }) \
     )
 
 // generic function on pointer is to validate parameter is array and not ptr.
