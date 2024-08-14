2ce499fe72f2254f709b907b690240c9f703b63f
Anubhav Rawal <rawalexe@amazon.com>
2024-08-14 12:55:12 -0700

    Support bash script generation for each lifecycle phase  (#393)

--- recipe2unit/main.py
+++ recipe2unit/main.py
@@ -1,10 +1,11 @@
 import getopt
 import yaml
-import json
 import os
 import sys
 
 global isRoot
+
+global recipe_runner_path
 
 
 def lower_keys(d):
@@ -40,35 +41,40 @@ def fillUnitSection(yaml_data):
     return unit_content
 
 
-def create_the_script_json_file(lifecycle, componentName):
-    with open(("./%s.json" % (componentName)), "w") as f:
-        json.dump(lifecycle, f)
-
-    print(componentName + ".json file generated successfully.")
-
-
-def fetch_script_section(lifecycle):
+def fetch_script_section(lifecycle, phase):
     global isRoot
-    runSection = lifecycle.get("run", "")
+    runSection = lifecycle.get(phase, "")
     execCommand = ""
 
     if isinstance(runSection, str):
         execCommand = runSection
     else:
-        scriptSection = runSection.get("Script", "")
+        scriptSection = runSection.get("script", "")
         execCommand = scriptSection
         if runSection.get("requiresprivilege", ""):
-            isRoot = True
+            isRoot = runSection.get("requiresprivilege", "")
     return execCommand
+
+
+def create_the_bash_script_file(script_section, filename):
+    try:
+        with open(filename, "w") as f:
+            f.write(script_section)
+        print(filename + " script file generated successfully.")
+    except Exception as error:
+        print(str(error))
 
 
 def fillServiceSection(yaml_data):
     global isRoot
+    global recipe_runner_path
+
     unit_content = "[Service]\n"
     unit_content += "Type=simple\n"
 
     # | "%t"	| Runtime directory root  |	This is either /run/ (for the system manager) or the path "$XDG_RUNTIME_DIR" resolves to (for user managers).
     unit_content += "WorkingDirectory= %t/" + yaml_data["componentname"] + "\n"
+    bash_script_file_name = "ggl." + yaml_data["componentname"] + ".script."
 
     platforms = yaml_data["manifests"]
     for platform in platforms:
@@ -83,16 +89,39 @@ def fillServiceSection(yaml_data):
                 lifecycleSection = yaml_data["lifecycle"]
                 lifecycleSection = lifecycleSection.get(platformSelection, {})
 
-            create_the_script_json_file(
-                lifecycleSection, yaml_data["componentname"]
+            create_the_bash_script_file(
+                fetch_script_section(lifecycleSection, "install"),
+                bash_script_file_name + "install",
             )
 
-            if isinstance(lifecycleSection.get("run", ""), str):
-                if lifecycleSection.get("run", "") == "":
+            run_phase_selection = ""
+
+            if lifecycleSection.get("startup", "") != "":
+                run_phase_selection = "startup"
+            else:
+                if lifecycleSection.get("run", "") != "":
+                    run_phase_selection = "run"
+                else:
                     return ""
 
-            scriptSection = fetch_script_section(lifecycleSection)
-            unit_content += "ExecStart=" + scriptSection + "\n"
+            create_the_bash_script_file(
+                fetch_script_section(lifecycleSection, run_phase_selection),
+                bash_script_file_name + run_phase_selection,
+            )
+
+            unit_content += (
+                "ExecStart="
+                + recipe_runner_path
+                + " -p "
+                + os.getcwd()
+                + "/"
+                + bash_script_file_name
+                + run_phase_selection
+                + "\n"
+            )
+            if run_phase_selection == "startup":
+                unit_content += "RemainAfterExit=true\n"
+                unit_content += "Type=oneshot\n"
             if isRoot:
                 unit_content += "User=root\n"
                 unit_content += "Group=root\n"
@@ -128,9 +157,10 @@ def getCommandArgs():
     options = "hr:"
 
     fileName = ""
+    recipeRunnerPath = ""
 
     # Long options
-    long_options = ["Help", "recipe-path="]
+    long_options = ["Help", "recipe-path=", "recipe-runner-path="]
 
     try:
         # Parsing argument
@@ -141,27 +171,37 @@ def getCommandArgs():
             if currentArgument in ("-h", "--Help"):
                 print("Displaying Help")
                 print(
-                    "-r / ----recipe-path <path to a recipe> | Provide path to the recipe file that needs to be processed"
+                    "-r / --recipe-path <path to a recipe> | Provide path to the recipe file that needs to be processed"
                 )
-
+                print(
+                    "-r / --recipe-runner-path <path to a recipe> | Provide absolute path to the recipe runner binary"
+                )
             elif currentArgument in ("-r", "--recipe-path"):
                 fileName = currentValue
+            elif currentArgument in ("-e", "--recipe-runner-path"):
+                recipeRunnerPath = currentValue
 
     except getopt.error as err:
         # output error, and return with an error code
         print(str(err))
 
-    return fileName
+    return (fileName, recipeRunnerPath)
 
 
 def main():
     global isRoot
+    global recipe_runner_path
+
     isRoot = False
 
-    filePath = getCommandArgs()
+    file_path, recipe_runner_path = getCommandArgs()
+
+    if file_path == "" or recipe_runner_path == "":
+        print("Error: file path or  recipe runner path not provided")
+
     unitComponentName = ""
 
-    with open(filePath, "r") as f:
+    with open(file_path, "r") as f:
         load_data = yaml.safe_load(f)
 
     # yaml_data = CaseInsensitiveDict(load_data)
@@ -170,7 +210,7 @@ def main():
     unitComponentName = yaml_data["componentname"]
 
     if systemd_unit != "":
-        with open(("./ggl%s.service" % unitComponentName), "w") as f:
+        with open(("./ggl.%s.service" % unitComponentName), "w") as f:
             f.write(systemd_unit)
         print(
             (
