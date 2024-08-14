cf7553f7d1280923cb969801d672482a3da17a09
Archit Gupta <archigup@amazon.com>
2024-08-13 17:31:00 -0700

    Add libsystemd

--- nix/package.nix
+++ nix/package.nix
@@ -8,13 +8,14 @@
 , argp-standalone
 , sqlite
 , libyaml
+, systemd
 , defaultMeta
 }:
 stdenv.mkDerivation {
   name = "aws-greengrass-lite";
   src = gglUtil.filteredSrc;
   nativeBuildInputs = [ pkg-config cmake ninja ];
-  buildInputs = [ openssl curl argp-standalone sqlite.dev libyaml.dev ];
+  buildInputs = [ openssl curl argp-standalone sqlite.dev libyaml.dev systemd ];
   cmakeBuildType = "MinSizeRel";
   cmakeFlags = gglUtil.fetchContentFlags ++ [ "-DENABLE_WERROR=1" ];
   meta = defaultMeta;
