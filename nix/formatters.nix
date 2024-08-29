995f1d889be5b3e5bfad41161ec16451cc3268ec
Archit Gupta <archigup@amazon.com>
2024-08-29 09:43:16 -0700

    Format clang config files

--- nix/formatters.nix
+++ nix/formatters.nix
@@ -1,7 +1,8 @@
-{ llvmPackages, cmake-format, yapf, ... }:
+{ llvmPackages, cmake-format, yapf, nodePackages, ... }:
 let
   fmt-c = "${llvmPackages.clang-unwrapped}/bin/clang-format -i";
   fmt-cmake = "${cmake-format}/bin/cmake-format -i";
+  fmt-yaml = "${nodePackages.prettier}/bin/prettier --write --parser yaml";
 in
 {
   "*.c" = fmt-c;
@@ -9,4 +10,5 @@ in
   "*.cmake" = fmt-cmake;
   "CMakeLists.txt" = fmt-cmake;
   "*.py" = "${yapf}/bin/yapf -i";
+  ".clang*" = fmt-yaml;
 }
