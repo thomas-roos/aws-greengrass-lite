c00a8091cda01f6f7a730019d503b0c25026bc1b
Archit Gupta <archigup@amazon.com>
2024-08-29 09:43:16 -0700

    Check included headers with clang-tidy

--- nix/checks/clang-tidy.nix
+++ nix/checks/clang-tidy.nix
@@ -3,47 +3,51 @@
 , gglUtil
 , ggl-clang
 , pkg-config
-, bear
 , cmake
 , clang-tools
 , ...
 }:
 let
-  c-files = map (p: lib.removePrefix ((toString src) + "/") (toString p))
-    (lib.fileset.toList (lib.fileset.fileFilter (file: file.hasExt "c") src));
+  inherit (lib) escapeRegex fileset removePrefix;
+  inherit (gglUtil) fetchContentFlags filteredSrc llvmStdenv;
+
+  c-files = map (p: removePrefix ((toString src) + "/") (toString p))
+    (fileset.toList (fileset.fileFilter (file: file.hasExt "c") src));
 
   build-dir = gglUtil.llvmStdenv.mkDerivation {
     name = "clang-tidy-build-dir";
-    src = gglUtil.filteredSrc;
     nativeBuildInputs = [ pkg-config clang-tools ];
     inherit (ggl-clang) buildInputs;
     buildPhase = ''
-      ${cmake}/bin/cmake -B $out -D CMAKE_BUILD_TYPE=Debug \
-        ${toString gglUtil.fetchContentFlags}
+      ${cmake}/bin/cmake -B $out -S ${filteredSrc} \
+        -D CMAKE_BUILD_TYPE=Debug ${toString fetchContentFlags}
       rm $out/CMakeFiles/CMakeConfigureLog.yaml
     '';
+    dontUnpack = true;
     dontPatch = true;
     dontConfigure = true;
     dontInstall = true;
     dontFixup = true;
   };
 
-  check-clang-tidy = file: gglUtil.llvmStdenv.mkDerivation {
+  check-clang-tidy = file: llvmStdenv.mkDerivation {
     name = "clang-tidy-${file}";
-    src = gglUtil.filteredSrc;
     nativeBuildInputs = [ pkg-config clang-tools ];
     inherit (ggl-clang) buildInputs;
     buildPhase = ''
-      clang-tidy -p ${build-dir} --quiet --warnings-as-errors='*' ${file}
+      clang-tidy -p ${build-dir} --quiet --warnings-as-errors='*' \
+        --header-filter='^${escapeRegex (toString filteredSrc)}"}' \
+        ${filteredSrc}/${file}
       touch $out
     '';
+    dontUnpack = true;
     dontPatch = true;
     dontConfigure = true;
     dontInstall = true;
     dontFixup = true;
   };
 in
-gglUtil.llvmStdenv.mkDerivation {
+llvmStdenv.mkDerivation {
   name = "check-clang-tidy";
   buildInputs = map check-clang-tidy c-files;
   installPhase = "touch $out";
