a592d93408641f3e6e817beb9459b44cecc010d9
Archit Gupta <archigup@amazon.com>
2024-09-06 12:04:03 -0700

    Remove strncat usage in gg-fleet-statusd

--- gg-fleet-statusd/src/fleet_status_service.c
+++ gg-fleet-statusd/src/fleet_status_service.c
@@ -9,27 +9,20 @@
 #include <ggl/error.h>
 #include <ggl/log.h>
 #include <ggl/object.h>
+#include <ggl/vector.h>
 #include <pthread.h>
-#include <string.h>
 #include <stdint.h>
 
-static const char TOPIC_START[] = "$aws/things/";
-static const char TOPIC_END[] = "/greengrassv2/health/json";
+#define MAX_THING_NAME_LEN 128
+
 #define TOPIC_PREFIX "$aws/things/"
 #define TOPIC_PREFIX_LEN (sizeof(TOPIC_PREFIX) - 1)
 #define TOPIC_SUFFIX "/greengrassv2/health/json"
 #define TOPIC_SUFFIX_LEN (sizeof(TOPIC_SUFFIX) - 1)
-#define MAX_THING_NAME_LEN 128
+
 #define TOPIC_BUFFER_LEN \
     (TOPIC_PREFIX_LEN + MAX_THING_NAME_LEN + TOPIC_SUFFIX_LEN)
 
-static const char PAYLOAD_START[]
-    = "{\"ggcVersion\":\"2.13.0\",\"platform\":\"linux\",\"architecture\":"
-      "\"amd64\",\"runtime\":\"NucleusLite\",\"thing\":\"";
-static const char PAYLOAD_END[]
-    = "\",\"sequenceNumber\":1,\"timestamp\":10,\"messageType\":\"COMPLETE\","
-      "\"trigger\":\"NUCLEUS_LAUNCH\",\"overallDeviceStatus\":\"HEALTHY\","
-      "\"components\":[]}";
 #define PAYLOAD_PREFIX \
     "{\"ggcVersion\":\"2.13.0\",\"platform\":\"linux\",\"architecture\":" \
     "\"amd64\",\"runtime\":\"NucleusLite\",\"thing\":\""
@@ -39,6 +32,7 @@ static const char PAYLOAD_END[]
     "\"trigger\":\"NUCLEUS_LAUNCH\",\"overallDeviceStatus\":\"HEALTHY\"," \
     "\"components\":[]}"
 #define PAYLOAD_SUFFIX_LEN (sizeof(PAYLOAD_SUFFIX) - 1)
+
 #define PAYLOAD_BUFFER_LEN \
     (PAYLOAD_PREFIX_LEN + MAX_THING_NAME_LEN + PAYLOAD_SUFFIX_LEN)
 
@@ -48,37 +42,42 @@ GglError publish_message(GglBuffer thing_name) {
     GGL_DEFER(pthread_mutex_unlock, mtx);
 
     // build topic name
-    static char topic[TOPIC_BUFFER_LEN + 1] = { 0 };
-
     if (thing_name.len > MAX_THING_NAME_LEN) {
         GGL_LOGE("fss", "Thing name too long.");
         return GGL_ERR_RANGE;
     }
 
-    strncat(topic, TOPIC_START, TOPIC_PREFIX_LEN);
-    strncat(topic, (char *) thing_name.data, MAX_THING_NAME_LEN);
-    strncat(topic, TOPIC_END, TOPIC_SUFFIX_LEN);
-
-    GglBuffer topic_name = { .data = (uint8_t *) topic, .len = strlen(topic) };
+    static uint8_t topic_buf[TOPIC_BUFFER_LEN];
+    GglByteVec topic_vec = GGL_BYTE_VEC(topic_buf);
+    GglError ret = ggl_byte_vec_append(&topic_vec, GGL_STR(TOPIC_PREFIX));
+    ggl_byte_vec_chain_append(&ret, &topic_vec, thing_name);
+    ggl_byte_vec_chain_append(&ret, &topic_vec, GGL_STR(TOPIC_SUFFIX));
+    if (ret != GGL_ERR_OK) {
+        return ret;
+    }
 
     // build payload
-    static char payload[PAYLOAD_BUFFER_LEN + 1] = { 0 };
-    strncat(payload, PAYLOAD_START, PAYLOAD_PREFIX_LEN);
-    strncat(payload, (char *) thing_name.data, MAX_THING_NAME_LEN);
-    strncat(payload, PAYLOAD_END, PAYLOAD_SUFFIX_LEN);
+    static uint8_t payload_buf[PAYLOAD_BUFFER_LEN];
+    GglByteVec payload_vec = GGL_BYTE_VEC(payload_buf);
+    ret = ggl_byte_vec_append(&topic_vec, GGL_STR(PAYLOAD_PREFIX));
+    ggl_byte_vec_chain_append(&ret, &topic_vec, thing_name);
+    ggl_byte_vec_chain_append(&ret, &topic_vec, GGL_STR(PAYLOAD_SUFFIX));
+    if (ret != GGL_ERR_OK) {
+        return ret;
+    }
 
-    GglBuffer msg = { .data = (uint8_t *) payload, .len = strlen(payload) };
-
-    GglError err = ggl_notify(
+    ret = ggl_notify(
         GGL_STR("aws_iot_mqtt"),
         GGL_STR("publish"),
         GGL_MAP(
-            { GGL_STR("topic"), GGL_OBJ(topic_name) },
-            { GGL_STR("payload"), GGL_OBJ(msg) }
+            { GGL_STR("topic"), GGL_OBJ(topic_vec.buf) },
+            { GGL_STR("payload"), GGL_OBJ(payload_vec.buf) }
         )
     );
-    if (err == GGL_ERR_OK) {
-        GGL_LOGI("Fleet Status Service", "Published update");
+    if (ret != GGL_ERR_OK) {
+        return ret;
     }
-    return err;
+
+    GGL_LOGI("Fleet Status Service", "Published update");
+    return GGL_ERR_OK;
 }
