f06cfca20b9541628dd1c1d8600d501297112a77
rawalexe <rawalexe@amazon.com>
2024-09-13 23:03:58 +0000

    Extend the parameters to test rest of the portion of recpie conversion

--- recipe2unit-test/src/entry.c
+++ recipe2unit-test/src/entry.c
@@ -11,8 +11,18 @@ GglError run_recipe2unit_test(void) {
     char recipe_path[] = "./recipe2unit_python/recipe.yml";
     char root_dir[] = ".";
 
+    args.gg_root_ca_path = "/home/test/rootCA.pem";
     args.recipe_path = recipe_path;
     args.root_dir = root_dir;
+    args.user = "ubuntu";
+    args.group = "ubuntu";
+    args.recipe_runner_path = "/home/recpierunner";
+    args.socket_path = "home/test/socket";
+    args.aws_container_auth_token = "AAAAAAAA";
+    args.ggc_version = "1.0.0";
+    args.thing_name = "test";
+    args.aws_container_cred_url = "12.43.1.1";
+    args.aws_region = "us-west-2";
 
     convert_to_unit(&args);
     return GGL_ERR_OK;
