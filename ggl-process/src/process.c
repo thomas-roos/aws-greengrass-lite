cac88f729a0f147c4d4a468e043cc00680086a89
Archit Gupta <archigup@amazon.com>
2024-10-02 09:42:24 -0700

    Remove PIDFD use
    PIDFDs are not needed for our usecase since we are only tracking direct
    children until we wait on them. Removing PIDFD use improves
    compatibility with older Linux kernels.

--- ggl-process/src/process.c
+++ ggl-process/src/process.c
@@ -6,51 +6,48 @@
 #include <sys/types.h>
 #include <assert.h>
 #include <errno.h>
+#include <ggl/defer.h>
 #include <ggl/error.h>
 #include <ggl/log.h>
 #include <limits.h>
 #include <linux/close_range.h>
-#include <linux/sched.h>
-#include <poll.h>
+#include <pthread.h>
 #include <signal.h>
 #include <sys/syscall.h>
 #include <sys/wait.h>
-#include <time.h>
 #include <unistd.h>
 #include <stdbool.h>
 #include <stdint.h>
 #include <stdlib.h>
 
-// Glibc 2.2.5 to 2.35 do not provide P_PIDFD
-// Yocto Kirkstone uses Glibc 2.35
-#define P_PIDFD 3
+static void sigalrm_handler(int s) {
+    (void) s;
+}
 
-static pid_t sys_clone3(struct clone_args *args) {
-    return (pid_t) syscall(SYS_clone3, args, sizeof(struct clone_args));
+// Lowest allowed priority in order to run before threads are created.
+__attribute__((constructor(101))) static void setup_sigalrm(void) {
+    sigset_t set;
+    sigemptyset(&set);
+    sigaddset(&set, SIGALRM);
+    int sys_ret = pthread_sigmask(SIG_BLOCK, &set, NULL);
+    if (sys_ret == -1) {
+        GGL_LOGE("process", "pthread_sigmask failed: %d", errno);
+        _Exit(1);
+    }
+
+    struct sigaction act = { .sa_handler = sigalrm_handler };
+    sigaction(SIGALRM, &act, NULL);
 }
 
 static int sys_close_range(unsigned first, unsigned last, unsigned flags) {
     return (int) syscall(SYS_close_range, first, last, flags);
 }
 
-static int sys_pidfd_send_signal(
-    int pidfd, int sig, siginfo_t *info, unsigned int flags
-) {
-    return (int) syscall(SYS_pidfd_send_signal, pidfd, sig, info, flags);
-}
-
 GglError ggl_process_spawn(char *const argv[], int *handle) {
     assert(argv[0] != NULL);
     assert(handle != NULL);
 
-    int pidfd = -1;
-    struct clone_args args = {
-        .pidfd = (uintptr_t) &pidfd,
-        .flags = CLONE_PIDFD | CLONE_CLEAR_SIGHAND,
-        .exit_signal = SIGCHLD,
-    };
-
-    pid_t pid = sys_clone3(&args);
+    pid_t pid = fork();
 
     if (pid == 0) {
         sys_close_range(3, UINT_MAX, CLOSE_RANGE_UNSHARE);
@@ -65,21 +62,14 @@ GglError ggl_process_spawn(char *const argv[], int *handle) {
         return GGL_ERR_FAILURE;
     }
 
-    if (pidfd < 0) {
-        // probably out of file descriptors?
-        GGL_LOGE("process", "Failed to obtain child pidfd.");
-        // Child leaks
-        return GGL_ERR_FAILURE;
-    }
-
-    *handle = pidfd;
+    *handle = pid;
     return GGL_ERR_OK;
 }
 
 GglError ggl_process_wait(int handle, bool *exit_status) {
     while (true) {
         siginfo_t info = { 0 };
-        int ret = waitid(P_PIDFD, (id_t) handle, &info, WEXITED);
+        int ret = waitid(P_PID, (id_t) handle, &info, WEXITED);
         if (ret < 0) {
             if (errno == EINTR) {
                 continue;
@@ -105,86 +95,53 @@ GglError ggl_process_wait(int handle, bool *exit_status) {
     }
 }
 
-static int ms_remaining(
-    struct timespec start, struct timespec now, int ms_requested
-) {
-    time_t sec_elapsed = now.tv_sec - start.tv_sec;
-    suseconds_t us_elapsed = now.tv_nsec - start.tv_nsec;
-    uint64_t ms_elapsed = (uint64_t) (sec_elapsed * 1000 + us_elapsed / 1000);
-    if (ms_elapsed > (uint64_t) ms_requested) {
-        return 0;
+GglError ggl_process_kill(int handle, uint32_t term_timeout) {
+    if (term_timeout == 0) {
+        kill(handle, SIGKILL);
+        return ggl_process_wait(handle, NULL);
     }
-    uint64_t ms_remaining = (uint64_t) ms_requested - ms_elapsed;
-    return (ms_remaining < INT32_MAX) ? (int) ms_remaining : INT32_MAX;
-}
 
-static GglError poll_wrapper(int handle, uint32_t timeout) {
-    assert(timeout <= INT32_MAX / 1000);
+    sigset_t set;
+    sigfillset(&set);
+    sigdelset(&set, SIGALRM);
 
-    struct timespec start = { 0 };
-    int ret = clock_gettime(CLOCK_MONOTONIC, &start);
-    if (ret < 0) {
-        GGL_LOGE("process", "Err %d when calling clock_gettime.", errno);
+    sigset_t old_set;
+
+    kill(handle, SIGTERM);
+
+    // Prevent multiple threads from unblocking SIGALRM
+    static pthread_mutex_t sigalrm_mtx = PTHREAD_MUTEX_INITIALIZER;
+
+    int waitid_ret;
+    int waitid_err;
+
+    {
+        pthread_mutex_lock(&sigalrm_mtx);
+        GGL_DEFER(pthread_mutex_unlock, sigalrm_mtx);
+
+        pthread_sigmask(SIG_SETMASK, &set, &old_set);
+
+        alarm(term_timeout);
+
+        siginfo_t info = { 0 };
+        waitid_ret = waitid(P_PID, (id_t) handle, &info, WEXITED);
+        waitid_err = errno;
+
+        alarm(0);
+
+        pthread_sigmask(SIG_SETMASK, &old_set, NULL);
+    }
+
+    if (waitid_ret == 0) {
+        return GGL_ERR_OK;
+    }
+
+    if (waitid_err != EINTR) {
+        GGL_LOGE("process", "Err %d when calling waitid.", errno);
         return GGL_ERR_FAILURE;
     }
 
-    int ms_timeout = (int) timeout * 1000;
-
-    struct pollfd poll_handle = { .fd = handle, .events = POLLIN };
-
-    while (true) {
-        ret = poll(&poll_handle, 1, ms_timeout);
-        if (ret < 0) {
-            if (errno == EINTR) {
-                struct timespec now;
-                ret = clock_gettime(CLOCK_MONOTONIC, &now);
-                if (ret < 0) {
-                    GGL_LOGE(
-                        "process", "Err %d when calling clock_gettime.", errno
-                    );
-                    return GGL_ERR_FAILURE;
-                }
-                ms_timeout = ms_remaining(start, now, ms_timeout);
-                start = now;
-                continue;
-            }
-
-            // Error
-            GGL_LOGE("process", "Err %d when calling poll.", errno);
-            return GGL_ERR_FAILURE;
-        }
-        if (ret > 0) {
-            return GGL_ERR_OK;
-        }
-        // Timed out
-        return GGL_ERR_RETRY;
-    }
-}
-
-GglError ggl_process_kill(int handle, uint32_t term_timeout) {
-    if (term_timeout == 0) {
-        int sys_ret = sys_pidfd_send_signal(handle, SIGKILL, NULL, 0);
-        if (sys_ret < 0) {
-            return GGL_ERR_FAILURE;
-        }
-    } else {
-        int sys_ret = sys_pidfd_send_signal(handle, SIGTERM, NULL, 0);
-        if (sys_ret < 0) {
-            return GGL_ERR_FAILURE;
-        }
-
-        GglError ret = poll_wrapper(handle, term_timeout);
-        if (ret != GGL_ERR_OK) {
-            if (ret != GGL_ERR_RETRY) {
-                return ret;
-            }
-
-            sys_ret = sys_pidfd_send_signal(handle, SIGKILL, NULL, 0);
-            if (sys_ret < 0) {
-                return GGL_ERR_FAILURE;
-            }
-        }
-    }
+    kill(handle, SIGKILL);
 
     return ggl_process_wait(handle, NULL);
 }
