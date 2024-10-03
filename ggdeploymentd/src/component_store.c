7be9b819c63e933006bd0c1c7d6c3f531aa2db29
Sameer Zuberi <samzub@amazon.com>
2024-10-03 13:38:46 -0700

    Add ability to retrieve list of components saved in the recipe directory (#494)

--- ggdeploymentd/src/component_store.c
+++ ggdeploymentd/src/component_store.c
@@ -5,6 +5,7 @@
 #include "component_store.h"
 #include <dirent.h>
 #include <fcntl.h>
+#include <ggl/alloc.h>
 #include <ggl/buffer.h>
 #include <ggl/bump_alloc.h>
 #include <ggl/core_bus/client.h>
@@ -12,8 +13,10 @@
 #include <ggl/error.h>
 #include <ggl/file.h>
 #include <ggl/log.h>
+#include <ggl/map.h>
 #include <ggl/object.h>
 #include <ggl/semver.h>
+#include <ggl/vector.h>
 #include <string.h>
 #include <stdbool.h>
 #include <stdint.h>
@@ -56,29 +59,20 @@ static GglError update_root_path(void) {
     return GGL_ERR_OK;
 }
 
-GglError find_available_component(
-    GglBuffer component_name, GglBuffer requirement, GglBuffer *version
+GglError retrieve_component_list(
+    int *out_fd, GglAlloc *alloc, GglMap *component_details
 ) {
     GglError ret = update_root_path();
     if (ret != GGL_ERR_OK) {
-        // we do not error out here because we will negotiate with the cloud if
-        // a local component is not found
-        GGL_LOGW(
-            "component-store",
-            "Failed to retrieve root path. Assuming no local component "
-            "available."
-        );
-        return GGL_ERR_NOENTRY;
+        GGL_LOGE("component-store", "Failed to retrieve root path.");
+        return GGL_ERR_FAILURE;
     }
 
     int root_path_fd;
     ret = ggl_dir_open(root_path, O_PATH, false, &root_path_fd);
     if (ret != GGL_ERR_OK) {
-        GGL_LOGW(
-            "component-store",
-            "Failed to open root_path. Assuming no local component available."
-        );
-        return GGL_ERR_NOENTRY;
+        GGL_LOGE("component-store", "Failed to open root_path.");
+        return GGL_ERR_FAILURE;
     }
     GGL_DEFER(ggl_close, root_path_fd);
 
@@ -87,25 +81,24 @@ GglError find_available_component(
         root_path_fd, GGL_STR("packages/recipes"), O_PATH, false, &recipe_dir_fd
     );
     if (ret != GGL_ERR_OK) {
-        GGL_LOGW(
-            "component-store",
-            "Failed to open recipe subdirectory. Assuming no local component "
-            "available."
-        );
-        return GGL_ERR_NOENTRY;
+        GGL_LOGE("component-store", "Failed to open recipe subdirectory.");
+        return GGL_ERR_FAILURE;
     }
 
-    // iterate through recipes in the directory to find the target component, if
-    // it exists
+    // iterate through recipes in the directory
     DIR *dir = fdopendir(recipe_dir_fd);
     if (dir == NULL) {
-        GGL_LOGW(
-            "component-store",
-            "Failed to open recipe directory. Assuming no local component "
-            "available."
-        );
-        return GGL_ERR_NOENTRY;
+        GGL_LOGE("component-store", "Failed to open recipe directory.");
+        return GGL_ERR_FAILURE;
     }
+
+    GglKV *kv_buffer = GGL_ALLOCN(alloc, GglKV, 256);
+    if (!kv_buffer) {
+        GGL_LOGE("component-store", "No memory available to allocate.");
+        return GGL_ERR_NOMEM;
+    }
+    GglKVVec components
+        = { .map = (GglMap) { .pairs = kv_buffer, .len = 0 }, .capacity = 256 };
 
     struct dirent *entry;
     // NOLINTNEXTLINE(concurrency-mt-unsafe)
@@ -132,35 +125,71 @@ GglError find_available_component(
         GglBuffer recipe_component
             = ggl_buffer_substr(GGL_STR(entry->d_name), 0, component_name_len);
 
-        // if the component names match, save the version of the component
-        if (ggl_buffer_eq(component_name, recipe_component)) {
-            // find the file extension length
-            size_t file_extension_len = 0;
-            char *dot_pos = NULL;
-            for (size_t i = strlen(entry->d_name); i > 0; i--) {
-                if (entry->d_name[i - 1] == '.') {
-                    dot_pos = entry->d_name + i - 1;
-                    file_extension_len = strlen(dot_pos + 1);
-                    break;
-                }
+        // find the file extension length
+        size_t file_extension_len = 0;
+        char *dot_pos = NULL;
+        for (size_t i = strlen(entry->d_name); i > 0; i--) {
+            if (entry->d_name[i - 1] == '.') {
+                dot_pos = entry->d_name + i - 1;
+                file_extension_len = strlen(dot_pos + 1);
+                break;
             }
+        }
 
-            // get the substring of the recipe file after the component name and
-            // before the file extension. This is the component version
-            GglBuffer recipe_version = ggl_buffer_substr(
-                GGL_STR(entry->d_name),
-                component_name_len,
-                strlen(entry->d_name) - file_extension_len
-            );
+        // get the substring of the recipe file after the component name and
+        // before the file extension. This is the component version
+        GglBuffer recipe_version = ggl_buffer_substr(
+            GGL_STR(entry->d_name),
+            component_name_len,
+            strlen(entry->d_name) - file_extension_len
+        );
 
-            bool requirement_satisfied
-                = is_contain(recipe_version, requirement);
+        GglKV component_info
+            = { .key = recipe_component, .val = GGL_OBJ(recipe_version) };
+        ggl_kv_vec_push(&components, component_info);
+    }
 
-            if (requirement_satisfied) {
-                // save the component information for the caller
-                *version = recipe_version;
+    GglObject obj = GGL_OBJ(*component_details);
+    ret = ggl_obj_deep_copy(&obj, alloc);
+    if (ret != GGL_ERR_OK) {
+        return ret;
+    }
+    *component_details = obj.map;
+    *out_fd = recipe_dir_fd;
+    return GGL_ERR_OK;
+}
+
+GglError find_available_component(
+    GglBuffer component_name, GglBuffer requirement, GglBuffer *version
+) {
+    int fd;
+    static uint8_t mem[256] = { 0 };
+    GglBumpAlloc balloc = ggl_bump_alloc_init(GGL_BUF(mem));
+    GglMap components;
+
+    GglError ret = retrieve_component_list(&fd, &balloc.alloc, &components);
+    if (ret != GGL_ERR_OK) {
+        return ret;
+    }
+
+    GGL_MAP_FOREACH(pair, components) {
+        if (ggl_buffer_eq(pair->key, component_name)) {
+            // if we find the desired component, save the version
+            if (pair->val.type != GGL_TYPE_BUF) {
+                GGL_LOGE(
+                    "component-store",
+                    "Component map contains invalid type for version."
+                );
+                return GGL_ERR_INVALID;
+            }
+            GglBuffer component_version = pair->val.buf;
+
+            // check if the version satisfies the requirement
+            if (is_contain(component_version, requirement)) {
+                *version = component_version;
             }
         }
     }
+
     return GGL_ERR_OK;
 }
