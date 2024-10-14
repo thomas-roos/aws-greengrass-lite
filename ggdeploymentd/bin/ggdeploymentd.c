a6d05778f413224782c8078659d6240b219e4e44
Archit Gupta <archigup@amazon.com>
2024-10-14 11:34:05 -0700

    Check argv[0] len in ggdeploymentd

--- ggdeploymentd/bin/ggdeploymentd.c
+++ ggdeploymentd/bin/ggdeploymentd.c
@@ -4,7 +4,10 @@
 
 #include "ggdeploymentd.h"
 #include <argp.h>
+#include <ggl/buffer.h>
 #include <ggl/error.h>
+#include <ggl/vector.h>
+#include <limits.h>
 #include <string.h>
 #include <stdbool.h>
 
@@ -29,18 +32,23 @@ static error_t arg_parser(int key, char *arg, struct argp_state *state) {
 
 static struct argp argp = { opts, arg_parser, 0, doc, 0, 0, 0 };
 
+static char bin_path[PATH_MAX];
+
 int main(int argc, char **argv) {
     if (argc < 1) {
         return 1;
     }
 
-    char bin_path[256] = { 0 };
-    strncat(bin_path, argv[0], strlen(argv[0]));
-    if (strlen(bin_path) < 1) {
+    GglByteVec bin_path_vec = GGL_BYTE_VEC(bin_path);
+    GglError ret = ggl_byte_vec_append(
+        &bin_path_vec, ggl_buffer_from_null_term(argv[0])
+    );
+    ggl_byte_vec_chain_push(&ret, &bin_path_vec, '\0');
+    if (ret != GGL_ERR_OK) {
         return 1;
     }
     bool slash_found = false;
-    for (size_t i = strlen(bin_path); i > 0; i--) {
+    for (size_t i = bin_path_vec.buf.len; i > 0; i--) {
         if (bin_path[i - 1] == '/') {
             bin_path[i] = '\0';
             slash_found = true;
@@ -54,7 +62,7 @@ int main(int argc, char **argv) {
     // NOLINTNEXTLINE(concurrency-mt-unsafe)
     argp_parse(&argp, argc, argv, 0, 0, NULL);
 
-    GglError ret = run_ggdeploymentd(bin_path);
+    ret = run_ggdeploymentd(bin_path);
     if (ret != GGL_ERR_OK) {
         return 1;
     }
