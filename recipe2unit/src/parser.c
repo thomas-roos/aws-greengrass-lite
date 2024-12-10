466da2526ec9df568367ee75752491ecad7f7e43
Anubhav Rawal <rawalexe@amazon.com>
2024-12-10 10:47:37 -0800

    Reduce warn level logs when expected phase isn't found (#711)

--- recipe2unit/src/parser.c
+++ recipe2unit/src/parser.c
@@ -97,7 +97,7 @@ GglError convert_to_unit(
         recipe_obj
     );
     if (ret != GGL_ERR_OK) {
-        GGL_LOGI("No recipe found");
+        GGL_LOGE("No recipe found");
         return ret;
     }
 
@@ -122,7 +122,7 @@ GglError convert_to_unit(
     }
 
     if (ret == GGL_ERR_NOENTRY) {
-        GGL_LOGW("No bootstrap phase present");
+        GGL_LOGD("No bootstrap phase present");
 
     } else if (ret != GGL_ERR_OK) {
         return ret;
@@ -154,7 +154,7 @@ GglError convert_to_unit(
     }
 
     if (ret == GGL_ERR_NOENTRY) {
-        GGL_LOGW("No Install phase present");
+        GGL_LOGD("No Install phase present");
 
     } else if (ret != GGL_ERR_OK) {
         return ret;
@@ -181,7 +181,7 @@ GglError convert_to_unit(
         RUN_STARTUP
     );
     if (ret == GGL_ERR_NOENTRY) {
-        GGL_LOGW("Neither run nor startup phase present");
+        GGL_LOGD("Neither run nor startup phase present");
     } else if (ret != GGL_ERR_OK) {
         return ret;
     } else {
@@ -201,6 +201,9 @@ GglError convert_to_unit(
         && existing_phases->has_run_startup == false) {
         GGL_LOGE("Recipes without at least 1 valid lifecycle step aren't "
                  "currently supported by GGLite");
+
+        GGL_LOGW("Note that in GG Lite, keys are case sensitive. Check the "
+                 "recipe reference for the correct casing.");
         return GGL_ERR_INVALID;
     }
 
