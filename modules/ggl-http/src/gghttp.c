600b70574274dce96fdb9f7696b9df48343d4fcd
Archit Gupta <archigup@amazon.com>
2025-02-10 17:50:39 -0800

    Add x-amz-content-sha256 header for s3 requests
    Older versions of curl do not add this header automatically.

--- modules/ggl-http/src/gghttp.c
+++ modules/ggl-http/src/gghttp.c
@@ -69,6 +69,13 @@ GglError sigv4_download(
     CurlData curl_data = { 0 };
     GglError error = gghttplib_init_curl(&curl_data, url_for_sigv4_download);
     if (error == GGL_ERR_OK) {
+        error = gghttplib_add_header(
+            &curl_data,
+            GGL_STR("x-amz-content-sha256"),
+            GGL_STR("UNSIGNED-PAYLOAD")
+        );
+    }
+    if (error == GGL_ERR_OK) {
         error = gghttplib_add_sigv4_credential(&curl_data, sigv4_details);
     }
     if (error == GGL_ERR_OK) {
