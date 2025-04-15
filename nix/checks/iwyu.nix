893f735172a098ba82aa1630d0546d5cecab6e81
Archit Gupta <archigup@amazon.com>
2025-04-15 14:57:42 -0700

    Speed up IWYU check

--- nix/checks/iwyu.nix
+++ nix/checks/iwyu.nix
@@ -2,44 +2,34 @@
 , gglUtil
 , ggl-clang
 , include-what-you-use
+, fd
 , ...
 }:
 let
-  inherit (gglUtil) llvmStdenv cFiles clangChecks;
-
-  check-iwyu = file: llvmStdenv.mkDerivation {
-    name = "iwyu-${file}";
-    nativeBuildInputs = [ include-what-you-use ];
-    inherit (ggl-clang) buildInputs;
-    buildPhase = ''
-      set -eo pipefail
-      white=$(printf "\e[1;37m")
-      red=$(printf "\e[1;31m")
-      clear=$(printf "\e[0m")
-      iwyu_tool.py -p ${clangChecks.cmakeBuildDir} \
-        ${clangChecks.src}/${file} -o clang -- \
-        -Xiwyu --error -Xiwyu --check_also="${clangChecks.src}/*" \
-        -Xiwyu --mapping_file=${src}/misc/iwyu_mappings.yml |\
-        { grep error: || true; } |\
-        sed 's|\(.*\)error:\(.*\)|'$white'\1'$red'error:'$white'\2'$clear'|' |\
-        sed 's|${clangChecks.src}/||'
-      touch $out
-    '';
-    dontUnpack = true;
-    dontPatch = true;
-    dontConfigure = true;
-    dontInstall = true;
-    dontFixup = true;
-    allowSubstitutes = false;
-  };
+  inherit (gglUtil) llvmStdenv clangChecks;
 in
 llvmStdenv.mkDerivation {
   name = "check-iwyu";
-  buildInputs = map check-iwyu cFiles;
-  installPhase = "touch $out";
+  nativeBuildInputs = [ include-what-you-use fd ];
+  inherit (ggl-clang) buildInputs;
+  buildPhase = ''
+    set -eo pipefail
+    white=$(printf "\e[1;37m")
+    red=$(printf "\e[1;31m")
+    clear=$(printf "\e[0m")
+    iwyu_tool.py -o clang -j $(nproc) -p ${clangChecks.cmakeBuildDir} \
+      $(fd . ${clangChecks.src}/ -e c) -- \
+      -Xiwyu --error -Xiwyu --check_also="${clangChecks.src}/*" \
+      -Xiwyu --mapping_file=${src}/misc/iwyu_mappings.yml |\
+      { grep error: || true; } |\
+      sed 's|\(.*\)error:\(.*\)|'$white'\1'$red'error:'$white'\2'$clear'|' |\
+      sed 's|${clangChecks.src}/||'
+    touch $out
+  '';
   dontUnpack = true;
   dontPatch = true;
   dontConfigure = true;
-  dontBuild = true;
+  dontInstall = true;
   dontFixup = true;
+  allowSubstitutes = false;
 }
